d0934c10e2ffbe623e4e7a16741b494661b313d8
jalvarez5625 <66888292+jalvarez5625@users.noreply.github.com>
2021-09-20 14:42:06 -0400

    Update RegisTeleOp.kt
    Added in-place rotation and extra motor power for swerve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.kt
@@ -9,15 +9,32 @@ class RegisTeleOp : RegisLinearOp() {
         while (opModeIsActive()) {
             val swerve = gamepad1.left_stick_x
             val thrust = gamepad1.left_stick_y.toDouble()
+            val spin = gamepad1.right_stick_x
             rightFrontMotor.power = thrust
             leftFrontMotor.power = thrust
             leftBackMotor.power = thrust
             rightBackMotor.power = thrust
             if (swerve > 0) {
                 leftBackMotor.power *= 1 - swerve
+                leftFrontMotor.power *= 1 - swerve
             } else if (swerve < 0) {
                 rightBackMotor.power *= 1 + swerve
+                rightFrontMotor.power *= 1 + swerve
             }
+
+            if (spin > 0) {
+                leftBackMotor.power *= 1 - spin
+                leftFrontMotor.power *= 1 - spin
+                rightBackMotor.power *= -1 * ( 1- spin)
+                rightFrontMotor.power *= -1 * ( 1- spin)
+            } else if (spin < 0) {
+                rightBackMotor.power *= 1 + swerve
+                rightFrontMotor.power *= 1 + swerve
+                rightBackMotor.power *= -1 * (1 + swerve)
+                rightFrontMotor.power *= -1 * (1 + swerve)
+
+            }
+
             telemetry.addData("Status", "Running")
             telemetry.update()
         }
