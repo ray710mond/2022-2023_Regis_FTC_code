a92606e349b55e0c0f6865b71173942a2dbb6b40
Raymond Provost <rprovost23@regis.org>
2023-02-03 15:56:01 -0500

    changed kotlin files to java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisAutonomousMode.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
+@Autonomous
+class RegisAutonomousMode : RegisLinearOp() {
+    override fun runOpMode() {
+        super.runOpMode()
+
+    }
+}
