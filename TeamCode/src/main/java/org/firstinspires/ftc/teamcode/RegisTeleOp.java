b8facd0fd0f300d414b4bd1d7c11903f4ecf159f
Raymond Provost <rprovost23@regis.org>
2023-02-05 02:00:30 -0500

    fixed through build in REV Hardware Client

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisTeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,17 +9,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @TeleOp(name="RegisTeleOp", group="Linear Opmode")
 public class RegisTeleOp extends RegisLinearOp {
-    double servoPosition = servo.servoHome;
+    double servoPosition = servoHome;
     final double servoSpeed = .01;
     public void runOpMode() {
         super.runOpMode();
-        while (OpModeIsActive()) {
+        while (opModeIsActive()) {
             double max;
             double rot = 0.5;
             double X = gamepad1.left_stick_x;
             double Y = -gamepad1.left_stick_y;
             double R = gamepad1.right_stick_x;
-            double LT = gamepad.left_trigger;
+            double LT = gamepad1.left_trigger;
 
 
             max = Math.abs(X*X) + Math.abs(Y*Y) + rot * Math.abs(R*R);
@@ -41,8 +42,6 @@ public class RegisTeleOp extends RegisLinearOp {
             telemetry.addData("Arm Motor Power: ", armMotor.getPower());
             telemetry.update();
             
-            servoPosition = Range.clip(servoPosition, servoMax, servoMin);
-            servo.setPosition(servoPosition);
             if (gamepad1.x){
                 servoPosition = servoOpen;
             }
